090c33d8eefab231d59d78742e7d542d786a5b36
omar badawy <omarbadawy10022@gmail.com>
2019-11-29 08:33:03 +0200

    Added the popup

--- js/index.js
+++ js/index.js
@@ -1,17 +1,39 @@
 const openSideBar = () => {
     document.querySelector('.side-bar').style.transform = 'translateX(0)';
     document.querySelector('.back-drop').style.display = 'block';
-}
+};
 
 const closeSideBar = () => {
     document.querySelector('.back-drop').style.display = 'none';
     document.querySelector('.side-bar').style.transform = 'translateX(-250px)';
-}
+};
+
+const openPopup = () => {
+    document.querySelector('.share-image__container').classList.add('share-image__container--visible');
+    document.body.style.overflow = 'hidden';
+};
+
+const closePopup = () => {
+    document.querySelector('.share-image__container').classList.remove('share-image__container--visible');
+    document.body.style.overflow = 'auto';
+};
+
+
+
+// aligning the popup due to the nav height
+
+const navHeight = document.querySelector('nav').clientHeight;
+document.querySelector('.share-image__container').style.top = navHeight + 'px';
+
+
+
+// envent listeners
 
 document.querySelector('.nav__side-box').addEventListener('click', openSideBar);
 
 document.querySelector('.back-drop').addEventListener('click', closeSideBar);
 
-let documentheight =document.body.clientHeight;
-document.querySelector('.side-bar').style.height = `${documentheight}px`;
+document.querySelector('.add-photo').addEventListener('click', openPopup);
+
+document.querySelector('.cancel-photo').addEventListener('click', closePopup);
 
